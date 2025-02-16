578a0d397ab180cf0e4542baa45e466c09cb384b
adilkadivala <adilkadivala560@gmail.com>
2025-02-16 10:37:34 +0530

    error build solve

--- client/components/clouds.tsx
+++ client/components/clouds.tsx
@@ -38,7 +38,7 @@ const Clouds = () => {
     <div className="py-5 md:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-lg/8 font-semibold text-gray-900 dark:text-zinc-200">
-          Trusted by the world's most innovative <span className="border-b-2 border-b-stone-500 dark:border-b-zinc-400 border-dashed">Startups</span> 
+          Trusted by the world&#39;s most innovative <span className="border-b-2 border-b-stone-500 dark:border-b-zinc-400 border-dashed">Startups</span> 
         </h2>
         
         <div className="mx-auto mt-20 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
